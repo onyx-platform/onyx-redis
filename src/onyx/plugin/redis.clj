94c4f11b979d917cbd7bce6794ecd056b5c1e1b7
gardnervickers <gardner@vickers.me>
2015-07-28 21:50:02 -0400

    WIP

--- src/onyx/plugin/redis.clj
+++ src/onyx/plugin/redis.clj
@@ -20,19 +20,16 @@
       (wcar conn (car/lpush key-queue k)))
     key-queue))
 
-(defn create-pending-state [event lifecycle]
-  (let [task (:onyx.core/task-map event)
-        keystore (:redis/keystore task)
-        conn (:redis/connection task)
-        keystore-queue (build-key-queue! conn (str keystore "-queue") keystore)]
-    {:redis/conn conn
-     :redis/keystore keystore
-     :redis/keystore-queue keystore-queue}))
+(defn inject-pending-state [event lifecycle]
+  (let [task (:onyx.core/task-map event)]
+    {:redis/conn (:redis/connection task)
+     :redis/keystore (:redis/keystore task)
+     :redis/pending-messages (atom {})}))
 
 (defmethod p-ext/read-batch :redis/read-from-set
-  [{:keys [onyx.core/task-map redis/conn redis/keystore redis/keystore-queue]}]
-  (let [task-map {:onyx/batch-size 5 :onyx/max-pending 10000 :onyx/batch-timeout 1000}
-        pending (wcar conn (car/llen keystore-queue))
+  [{:keys [onyx.core/task-map redis/conn redis/keystore redis/pending-messages]}]
+  (let [task-map {:onyx/batch-size 500 :onyx/max-pending 10000 :onyx/batch-timeout 1000}
+        pending (count @pending-messages)
         max-pending (arg-or-default :onyx/max-pending task-map)
         batch-size (:onyx/batch-size task-map)
         max-segments (min (- max-pending pending) batch-size)
@@ -40,52 +37,37 @@
         batch-end-time (+ ms (System/currentTimeMillis))
         batch (if (pos? max-segments)
                 (loop [segments [] cnt 0]
+
                   (if (or (= cnt max-segments)
                           (neg? (- batch-end-time (System/currentTimeMillis))))
                     segments
-                    (let [next-key (wcar conn (car/rpoplpush keystore-queue keystore-queue))
-                          message (wcar conn (car/smembers next-key))]
+                    (let [key (wcar conn (car/rpoplpush keystore keystore))
+                          message (wcar conn (car/smembers key))]
                       (if message
                         (recur (conj segments
-                                     {:id next-key
+                                     {:id key
                                       :input :redis
-                                      :message {next-key message}})
+                                      :message message})
                                (inc cnt))
                         segments)))))]
+    (doseq [m batch]
+      (swap! pending-messages assoc (:id m) (:message m)))
     {:onyx.core/batch batch}))
 
 (defmethod p-ext/ack-message :redis/read-from-set
-  [{:keys [redis/conn redis/keystore redis/keystore-queue]} message-id]
-  (wcar conn (car/lrem keystore-queue 0 message-id)))
+  [{:keys [redis/conn redis/keystore redis/pending-messages]} message-id]
+  (wcar conn (car/lrem keystore 0 message-id))
+  (swap! pending-messages dissoc message-id))
 
-(defmethod p-ext/retry-message :redis/read-from-set ;; fix these to be actually stateless...
-  [{:keys [redis/conn redis/keystore-queue]} message-id]
-  (wcar conn (car/lpush keystore-queue message-id)))
+(defmethod p-ext/retry-message :redis/read-from-set
+  [{:keys [redis/conn]} message-id]
+  (comment "Dont need this"))
 
 (defmethod p-ext/pending? :redis/read-from-set
   [{:keys []} message-id]
   (get @pending-messages message-id))
 
 (defmethod p-ext/drained? :redis/read-from-set
-  [])
-
-;;;; Steps for using a multi-tenant hashmap in redis.
-;;;;;; In order to achieve multi-tenant hashmaps with a low probability of key collision,
-;;;;;; namespace your key's, and store all the namespaced keys in a single set.
-;;;;;; The onyx-redis plugin will take the the name of a key, that links to the set of keys.
-;;;;;; Each key read will become a segment tagged as {:key contents}.
-;;;;;;
-;;;;;; To achieve this, we place all the keys from the key set into a linked list. This
-;;;;;; gives us explicit ordering.
-;;;;;;
-;;;;;;
-
-
-                                        ;(get-next-item :df 1)
-
-                                        ;(keys @pending-messages)
-                                        ;(deref (future (get-next-item "clarity.functions.denver-health/keystore")) 10000 nil)
-
-
-(let
-    {:onyx.core/batch batch})
+  [{:keys [redis/conn redis/keystore redis/pending-messages]}]
+  (and (= (count @pending-messages) 1)
+       (zero? (wcar conn (car/llen keystore)))))
