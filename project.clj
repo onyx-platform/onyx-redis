3bd460abd0a6d9bac82554df1d33fbe01c6fc133
gardnervickers <gardner@vickers.me>
2015-08-07 10:45:22 -0400

    updated to clojure 1.7, onyx 0.7

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject onyx-redis "0.6.0.7"
+(defproject onyx-redis "0.7.0.0"
   :description "Onyx plugin for redis"
   :url "FIX ME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.onyxplatform/onyx "0.6.0"]
-                 [com.taoensso/carmine "2.11.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.onyxplatform/onyx "0.7.0"]
+                 [com.taoensso/carmine "2.11.1" :exclusions [com.taoensso/timbre]]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [midje "1.7.0"]
-                 [com.taoensso/timbre "3.4.0"]]
+                 [midje "1.7.0"]]
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"])
