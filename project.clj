c5d05e52263f7b57898573820d7b33cb14bc9b89
Lucas Bradstreet <lucas.bradstreet@onyxplatform.org>
2015-09-07 11:52:43 +0200

    Add midje and co to dev plugins/dependencies

--- project.clj
+++ project.clj
@@ -6,6 +6,10 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.onyxplatform/onyx "0.7.0"]
                  [com.taoensso/carmine "2.11.1" :exclusions [com.taoensso/timbre]]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [midje "1.7.0"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]]
+                   :plugins [[lein-midje "3.1.3"]
+                             [lein-set-version "0.4.1"]
+                             [lein-pprint "1.1.1"]]}
+             :circle-ci {:jvm-opts ["-Xmx4g"]}}
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"])
