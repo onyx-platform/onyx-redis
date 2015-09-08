7600103df77967ead6777d8051e53e930231da8d
Lucas Bradstreet <lucas.bradstreet@onyxplatform.org>
2015-09-08 23:04:55 +0200

    Add repositories to project

--- project.clj
+++ project.clj
@@ -3,6 +3,14 @@
   :url "https://github.com/onyx-platform/onyx-redis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"snapshots" {:url "https://clojars.org/repo"
+                              :username :env
+                              :password :env
+                              :sign-releases false}
+                 "releases" {:url "https://clojars.org/repo"
+                             :username :env
+                             :password :env
+                             :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.7.3"]
