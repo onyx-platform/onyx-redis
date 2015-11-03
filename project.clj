8dae13df3779f9a9930585680f8682884fbc1b42
Michael Drogalis <mjd3089@rit.edu>
2015-11-03 13:13:40 -0800

    Exclude Encore.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.8.0-SNAPSHOT"]
-                 [com.taoensso/carmine "2.11.1" :exclusions [com.taoensso/timbre]]
+                 [com.taoensso/carmine "2.11.1" :exclusions [com.taoensso/timbre com.taoensso/encore]]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.3"]
