797dc99fb6a4f4f1ac0bc802976f760120b75b02
Gardner Vickers <gardner@vickers.me>
2016-05-04 14:20:01 -0400

    Updated or new task bundle scheam merge behavior

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.9.4"]
+                 [org.onyxplatform/onyx "0.9.5-SNAPSHOT"]
                  [com.taoensso/carmine "2.12.2" :exclusions [com.taoensso/timbre com.taoensso/encore]]
                  [aero "0.2.0"]]
   :profiles {:dev {:dependencies []
