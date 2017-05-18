d9c9c0e039d2792a71887b68419216fd475e8eed
OnyxBot <michael.drogalis@onyxplatform.org>
2017-05-18 01:07:34 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-redis "0.10.0.0-beta16"
+(defproject org.onyxplatform/onyx-redis "0.10.0.0-SNAPSHOT"
   :description "Onyx plugin for redis"
   :url "https://github.com/onyx-platform/onyx-redis"
   :license {:name "Eclipse Public License"
